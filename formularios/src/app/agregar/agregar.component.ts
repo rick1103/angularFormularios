109c9f457d806ca1e7680b80a91e53d3c7e78c7b
Ricardo García <rjozze10@gmail.com>
2021-04-21 23:21:09 -0600

    68. Maquetar Pagina, 69. FormBuilder, 70. validar

---
+++ formularios/src/app/agregar/agregar.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Usuario{
+  nombre: string;
+  correo: string;
+  password: string;
+}
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styleUrls: ['./agregar.component.scss']
+})
+export class AgregarComponent implements OnInit {
+  
+  formularioCreado: FormGroup;
+  usuarios: Array<Usuario> = new Array<Usuario>();
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.crearFormulario()
+  }
+
+  crearFormulario()
+  {
+    this.formularioCreado = this.formBuilder.group({
+      nombre: ['Ricardo', Validators.required],
+      correo: ['', Validators.compose([
+        Validators.required, Validators.email
+      ])],
+      password: ['', Validators.compose([
+        Validators.required, Validators.minLength(8)
+      ])]
+    })
+  }
+
+  agregar()
+  {
+    this.usuarios.push(this.formularioCreado.value as Usuario)
+    this.formularioCreado.reset()
+  }
+
+}
